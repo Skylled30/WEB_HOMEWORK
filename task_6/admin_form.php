14ea322e5601c652f31b0f77d44e43926501ffaf
Alexandr Vanteev <vanteevs@mail.ru>
2019-06-27 17:25:57 +0800

    Update admin_form.php

--- task_6/admin_form.php
+++ task_6/admin_form.php
@@ -1,12 +1,12 @@
 <!DOCTYPE HTML>
 <html>
- <head>
-  <meta charset="utf-8">
-  <title>Панель администратора</title>
- </head>
- <body>
- 	<div>
- 		<?php
+<head>
+	<meta charset="utf-8">
+	<title>Панель администратора</title>
+</head>
+<body>
+	<div>
+		<?php
 			if(empty($_POST['f'])){ 
 				echo "<h2>Вы ничего не выбрали</h2>";
 			} 
@@ -16,18 +16,18 @@
 				$af=$_POST['f'];
 				$n=count($af);
 				for($i=0;$i<$n;$i++){
-					if(substr($af[$i],-1)==1){
-						for($j=0;$j<sizeof($file);$j++){
-							if(substr($file[$j], 0,1)==substr($af[$i], 0,1)){
-								$file[$j]=substr($af[$i],0,-1)."0".PHP_EOL;
-								echo substr($file[$j],0,-3)."<br>";
-								} 
-							}
-						} 
-					}
-				file_put_contents("logs/form1.txt", $file);	
+				if(substr($af[$i],-1)==1){
+					for($j=0;$j<sizeof($file);$j++){
+						if(substr($file[$j], 0,1)==substr($af[$i], 0,1)){
+							$file[$j]=substr($af[$i],0,-1)."0".PHP_EOL;
+							echo substr($file[$j],0,-3)."<br>";
+							} 
+						}
+					} 
 				}
+			file_put_contents("logs/form1.txt", $file);	
+			}
 		?>
 	</div>
- </body>
+</body>
 </html>
